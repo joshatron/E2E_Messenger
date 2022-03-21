d58a4a828723c5ccc4bacab209714735dc069678
Joshua Leger <legermjoshua@gmail.com>
2022-03-20 21:47:24 -0700

    Worked on cleaning up signature functions

--- e2emessenger/crypto/signature.py
+++ e2emessenger/crypto/signature.py
@@ -8,25 +8,32 @@ def current_date_time():
     return datetime.now()
 
 
-def signature_message(username, date_time):
+def __signature_message(username, date_time):
     return (username + " " + date_time.isoformat()).encode('utf-8')
 
 
+def __padding():
+    return padding.PSS(mgf=padding.MGF1(hashes.SHA256()),
+                       salt_length=padding.PSS.MAX_LENGTH),
+
+
+def __hash():
+    return hashes.SHA256()
+
+
 def generate_signature(private_key, username, date_time):
-    return private_key.sign(signature_message(username=username, date_time=date_time),
-                            padding.PSS(mgf=padding.MGF1(hashes.SHA256()),
-                                        salt_length=padding.PSS.MAX_LENGTH),
-                            hashes.SHA3_256())
+    return private_key.sign(__signature_message(username=username, date_time=date_time),
+                            __padding(),
+                            __hash())
 
 
 def check_signature(public_key, signature, username, date_time):
     try:
         public_key.verify(signature,
-                          signature_message(
+                          __signature_message(
                               username=username, date_time=date_time),
-                          padding.PSS(mgf=padding.MGF1(hashes.SHA256()),
-                                      salt_length=padding.PSS.MAX_LENGTH),
-                          hashes.SHA3_256())
+                          __padding(),
+                          __hash())
         return True
     except InvalidSignature:
         return False
