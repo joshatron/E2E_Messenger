3e87721c12427fd2c41c192a9d1eef0f0dd699a7
Joshua Leger <legermjoshua@gmail.com>
2022-03-24 18:34:11 -0700

    Started working on cliend

--- e2emessenger/client/client.py
+++ e2emessenger/client/client.py
@@ -1,3 +1,22 @@
+from dao import FileBasedClientDAO
 from ..crypto import keys
 from ..crypto import signature
 from ..crypto import message
+import requests
+
+dao = FileBasedClientDAO("client_data")
+server_url = "http://localhost:8080"
+
+(u, k) = dao.load_user_data()
+
+username = ""
+keypair = None
+
+if len(u) == 0:
+    username = "Joshua"
+    keypair = keys.generate_keypair()
+    r = requests.put(server_url + "/v1/user/register", data={
+                     "username": username, "public_key": keys.export_public_key(keypair.public_key())})
+else:
+    username = u
+    keypair = keys.import_keypair(k)
